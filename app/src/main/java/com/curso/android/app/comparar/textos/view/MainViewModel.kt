1cdc78f2e71df176b1a969e17f6bb22ad5abb4f7
geraldineac <geraldineacevedo29@gmail.com>
2023-09-02 01:16:34 -0300

    cambios en el ViewModel

--- app/src/main/java/com/curso/android/app/comparar/textos/view/MainViewModel.kt
+++ app/src/main/java/com/curso/android/app/comparar/textos/view/MainViewModel.kt
@@ -1,7 +1,28 @@
 package com.curso.android.app.comparar.textos.view
 
+import android.os.Parcel
+import android.os.Parcelable
 import androidx.lifecycle.ViewModel
 
-class MainViewModel: ViewModel() {
+class MainViewModel() : ViewModel(), Parcelable {
+    constructor(parcel: Parcel) : this() {
+    }
 
+    override fun writeToParcel(parcel: Parcel, flags: Int) {
+
+    }
+
+    override fun describeContents(): Int {
+        return 0
+    }
+
+    companion object CREATOR : Parcelable.Creator<MainViewModel> {
+        override fun createFromParcel(parcel: Parcel): MainViewModel {
+            return MainViewModel(parcel)
+        }
+
+        override fun newArray(size: Int): Array<MainViewModel?> {
+            return arrayOfNulls(size)
+        }
+    }
 }
